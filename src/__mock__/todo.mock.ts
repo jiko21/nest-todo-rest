510ccb741b60f48643a5ec9be82555e036e155c5
Daiki Kojima <daikikojima0531@icloud.com>
2020-02-12 00:01:28 +0900

    A: mock dir

---
+++ src/__mock__/todo.mock.ts
@@ -0,0 +1,17 @@
+import { ITodo } from '../todos/todo.interface';
+import { TodoDao } from '../dao/Todo.dao';
+
+export const TODOS_ARAY: ITodo[] = [
+  {
+    id: 1,
+    title: 'rakuraku',
+  },
+  {
+    id: 2,
+    title: 'jest',
+  },
+];
+
+export const TODOS: TodoDao = {
+  title: 'Bitbucket',
+};
